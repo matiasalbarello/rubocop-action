df2d580bf0eff7ea82c4bcfeb91c23ec372f49a0
Matias <matias.albarello@gmail.com>
2021-01-29 18:15:34 +0100

    Try get PR number from Event Path

--- entrypoint.rb
+++ entrypoint.rb
@@ -3,18 +3,18 @@
 
 require_relative './app/services/main_app_service'
 require 'octokit'
-event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
-pull_request = event['pull_request']
 
-p event # debug
-return unless pull_request
+pull_request_nr = `PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')`
+
+p pull_request_nr # debug
+return unless pull_request_nr
 
 MainAppService.configure do |config|
   config.client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
   config.ref = ENV['GITHUB_SHA']
   config.repo = ENV['GITHUB_REPOSITORY']
   config.workspace = ENV['GITHUB_WORKSPACE'] #  a copy of your repository if your workflow uses the actions/checkout action
-  config.pr_id = pull_request['number'].to_i
+  config.pr_id = pull_request_nr.to_i
 end
 
 MainAppService.new.call
